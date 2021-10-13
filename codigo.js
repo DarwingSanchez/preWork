9ba330184d1e31ef5e05ed3333d706ee8c6adf36
DarwingSanchez <darwing.sanchez@hotmail.com>
2021-10-13 14:59:38 -0500

    Se eliminó el body del documento

--- codigo.js
+++
@@ -1,8 +0,0 @@
-var peso = prompt("Cual es tu peso?")
-        var gravedadTierra = 9.8
-        var gravedadMarte = 3.7
-        var gravedadJupiter = 24.8
- 
-        document.write("\nTu peso en jupiter es <strong>" + parseFloat((peso * gravedadJupiter)/gravedadTierra).toFixed(2) + "Kg </strong>");
-        document.write("\n & Tu peso en la Marte es <strong>" + parseFloat((peso* gravedadMarte)/gravedadTierra).toFixed(2) + "Kg </strong>");
-
