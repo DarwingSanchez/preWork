a40807b3ed462cd84f31903b5e185a908834110e
DarwingSanchez <darwing.sanchez@hotmail.com>
2021-10-13 11:23:42 -0500

    Eliminacion de archivos no necesarios

--- servidor/servidor.js
+++
@@ -1,9 +0,0 @@
-var  express = require('express')
-var app = express()
- 
-app = app.get('/', inicio) 
-function inicio(request, result){
-    result.send("HOLA COMO ESTA: "); 
-}
- 
-app.listen(8989)
